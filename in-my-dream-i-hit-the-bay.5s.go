047fac70628c51bc79eb51f56a6568e78f32f951
Elder  Rapper <elderrapper@gmail.com>
2022-06-02 21:45:24 +0800

    Use embed package to include the dependent files during compilation

--- in-my-dream-i-hit-the-bay.5s.go
+++ in-my-dream-i-hit-the-bay.5s.go
@@ -1,4 +1,3 @@
-//usr/bin/env go run $0 $@; exit
 package main
 
 // <xbar.title>In My Dream I Hit The Bay</xbar.title>
@@ -11,18 +10,24 @@ package main
 // <xbar.abouturl>https://github.com/davidhsingyuchen/xbar-plugin-in-my-dream-i-hit-the-bay</xbar.abouturl>
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 
 	"github.com/leaanthony/go-ansi-parser"
 )
 
 const (
+	secondEntryText = "thoughts"
+)
+
+var (
 	// The .B64 file is obtained by base64-encoding this image:
 	// https://img.favpng.com/11/18/10/tightrope-walking-computer-icons-youtube-png-favpng-qcQdAyeKgpbCPdmQeaU9757iD.jpg
-	manWalkingOnATightropeB64Path = `man-walking-on-a-tightrope.B64`
+	//go:embed man-walking-on-a-tightrope.B64
+	manWalkingOnATightropeB64 string
+
 	// To get the .ans file, follow these steps:
 	// 1. Download https://commons.wikimedia.org/wiki/File:Stack_Overflow_icon.svg.
 	//    The file extension will be png instead of svg
@@ -33,11 +38,11 @@ const (
 	//    and the ANSI parser will complain about that.
 	// 3. Convert th e.jpg file to be an .ans one using
 	//    https://manytools.org/hacker-tools/convert-images-to-ascii-art/.
-	stackOverflowLogoPath = "stack-overflow-logo.ans"
-	secondEntryText       = "thoughts"
-)
+	//go:embed "stack-overflow-logo.ans"
+	stackOverflowLogo string
 
-var firstEntryAlternatingTexts = []string{"Everything will be fine.", "Nah, it's just a fairy tale."}
+	firstEntryAlternatingTexts = []string{"Everything will be fine.", "Nah, it's just a fairy tale."}
+)
 
 func mustBeNil(err error, msg string) {
 	if err != nil {
@@ -45,9 +50,7 @@ func mustBeNil(err error, msg string) {
 	}
 }
 
-func printThumbnail(path string) {
-	thumbnail, err := os.ReadFile(path)
-	mustBeNil(err, "failed to read the thumbnail from the file system")
+func printThumbnail(thumbnail string) {
 	fmt.Printf("| templateImage=%s\n", string(thumbnail))
 }
 
@@ -56,22 +59,18 @@ func printFirstEntry(texts []string) {
 	fmt.Println(texts[idx])
 }
 
-func printSecondEntry(text, imgPath string) {
+func printSecondEntry(text, img string) {
 	fmt.Println(text)
 
-	img, err := os.ReadFile(imgPath)
-	mustBeNil(err, "failed to read the image for the second entry from the file system")
-
-	str := string(img)
 	start := 0
-	for i := 0; i < len(str); i++ {
-		if str[i] == '\n' {
+	for i := 0; i < len(img); i++ {
+		if img[i] == '\n' {
 			// The ansi library is used because
 			// it reset all modes (i.e., ESC[0m) at the end of each ANSI sequence.
 			// The implication is that when printing '--',
 			// those strings won't be stylized by the last ANSI sequence
 			// as the original .ans file does not necessarily reset all modes at the end of each sequence.
-			row, err := ansi.Parse(str[start:i])
+			row, err := ansi.Parse(img[start:i])
 			mustBeNil(err, "failed to parse the row into ANSI sequences")
 
 			fmt.Printf("--%s\n", ansi.String(row))
@@ -85,9 +84,9 @@ func printSeparator() {
 }
 
 func main() {
-	printThumbnail(manWalkingOnATightropeB64Path)
+	printThumbnail(manWalkingOnATightropeB64)
 	printSeparator()
 	printFirstEntry(firstEntryAlternatingTexts)
 	printSeparator()
-	printSecondEntry(secondEntryText, stackOverflowLogoPath)
+	printSecondEntry(secondEntryText, stackOverflowLogo)
 }
